996881b3adafd1cb9589d80567c87de78ce644e6
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-06-28 15:58:50 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,13 @@
 module Players
   class Computer < Player
     def move(board)
+      computer_move = rand(1...9)
     end
+
+    def valid?(position)
+      position.to_i.between?(1, 9) && !taken?(position)
+    end
+
   end
 
 
